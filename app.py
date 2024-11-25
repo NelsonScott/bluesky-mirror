475af9c53a2b89dec3e7bcc6cc6500aaed0c67df
Scott Nelson <scottdnelson.coffee@gmail.com>
2024-11-24 21:00:39 -0500

    If deployed, get from env var secret key

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import json
+import os
 from flask import Flask, redirect, request, render_template, session, url_for
 from process_tweet import post_to_bluesky
 
 app = Flask(__name__)
-app.secret_key = json.load(open("credentials.json"))['secret_key']
+app.secret_key = os.environ.get('secret_key') or json.load(open("credentials.json"))['secret_key']
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
